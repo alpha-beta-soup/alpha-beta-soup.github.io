f5e87476d5a4221b50893a0510113257f117e903
Richard Law <richard.m.law@gmail.com>
2018-12-10 22:41:02 +1300

    fix next and previous links in blog template

--- blag/src/templates/blog-post.js
+++ blag/src/templates/blog-post.js
@@ -64,7 +64,7 @@ class BlogPostTemplate extends React.Component {
           <li>
             {
               previous &&
-              <Link to={previous.fields.slug} rel="prev">
+              <Link to={previous.frontmatter.path} rel="prev">
                 ← {previous.frontmatter.title}
               </Link>
             }
@@ -72,7 +72,7 @@ class BlogPostTemplate extends React.Component {
           <li>
             {
               next &&
-              <Link to={next.fields.slug} rel="next">
+              <Link to={next.frontmatter.path} rel="next">
                 {next.frontmatter.title} →
               </Link>
             }
