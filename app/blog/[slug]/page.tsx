70a81f59b8be9f777390964f4ce168b7f4315d98
Richard Law <lawr@landcareresearch.co.nz>
2026-04-19 21:18:05 +1200

    revisting existing post with recently added blog components

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -86,6 +86,11 @@ export default function Blog({ params }) {
       <h1 className="title font-semibold text-2xl tracking-tighter">
         {post.metadata.title}
       </h1>
+      {post.metadata.subtitle && (
+        <p className="text-lg text-neutral-500 dark:text-neutral-400 mt-1 tracking-tight">
+          {post.metadata.subtitle}
+        </p>
+      )}
       <div className="flex justify-between items-center mt-2 mb-8 text-sm">
         <p className="text-sm text-neutral-600 dark:text-neutral-400">
           {formatDate(post.metadata.publishedAt)}
